c821aef19b0ae1fe0f199fb04197105b5b56f46b
Michael.K <michael34435@capslock.tw>
2018-08-27 02:10:06 +0800

    chore: Add special ticket info

--- php/resources/views/event/show.blade.php
+++ php/resources/views/event/show.blade.php
@@ -27,7 +27,7 @@
                         現場票
                       </span>
                       <p class="ticket-descripton mb0 text-muted" property="description">
-                        到現場以現金支付新台幣 {{ $event->price }} 元
+                        到現場以現金支付新台幣 {{ $event->price }} 元 {{ $event->discount_rate ? '(學生優惠: 新台幣 '.((1 - $event->discount_rate) * $event->price).' 元)' : '' }}
                       </p>
                     </td>
                     <td style="width:180px; text-align: right;">
