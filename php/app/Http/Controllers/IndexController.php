6f06d390a0ddfec9740d47e0ed1f7b3ee3e41bf4
Michael.K <michael34435@capslock.tw>
2018-08-26 22:25:24 +0800

    fix: Remove non-sense code...

--- php/app/Http/Controllers/IndexController.php
+++ php/app/Http/Controllers/IndexController.php
@@ -7,8 +7,6 @@ class IndexController extends Controller
 
     public function index()
     {
-        throw new \Exception("Error Processing Request", 1);
-
         return redirect()->action('EventController@index');
     }
 }
