c29fbd45d9d184f98e102e8c51d6eb9ab54e9665
Michael.K <michael34435@capslock.tw>
2018-08-27 15:04:37 +0800

    feat: Add disqus to event page

--- php/resources/views/event/show.blade.php
+++ php/resources/views/event/show.blade.php
@@ -133,7 +133,7 @@
     </div>
   </section>
   @if($event->address)
-    <section id="location" class="container p0">
+    <section id="location" class="container">
       <div class="row">
         <div class="col-md-12">
           <div class="google-maps content">
@@ -143,4 +143,27 @@
       </div>
     </section>
   @endif
+  <section class="container p0">
+    <div id="disqus_thread"></div>
+  </section>
+
+  <script>
+
+  /**
+  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+  /*
+  var disqus_config = function () {
+  this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+  this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+  };
+  */
+  (function() { // DON'T EDIT BELOW THIS LINE
+  var d = document, s = d.createElement('script');
+  s.src = 'https://events-capslock-tw.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+  })();
+  </script>
+  <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 @endsection
