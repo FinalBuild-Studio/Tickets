bbaf9fa604318012e119159ff49f25be75436218
Michael.K <michael34435@capslock.tw>
2018-08-29 16:54:43 +0800

    chore: Mark as red

--- php/resources/views/mails/order/change.blade.php
+++ php/resources/views/mails/order/change.blade.php
@@ -6,7 +6,7 @@
   感謝您參加了大寫鎖定工作室所舉辦的活動「{{ $order->event->name }}」
 </p>
 <p>
-  因為活動報名踴躍以及場地無法預期租/借，將活動場地改為 {{ $order->event->address }} {{ $order->event->place }}
+  因為活動報名踴躍以及場地無法預期租/借，將活動場地改為 <span style="color: red;">{{ $order->event->address }} {{ $order->event->place }}</span>
 </p>
 <p>
   若是因為活動場地無法出席者，請透過 Telegram 或是直接在 Disqus 留下您的訂單編號，會幫您進行處理
