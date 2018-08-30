f447d7b1b61d79e652ceb6309d55e5cd94d91959
Michael.K <michael34435@capslock.tw>
2018-08-30 16:33:03 +0800

    chore: Change icon

--- php/resources/views/event/index.blade.php
+++ php/resources/views/event/index.blade.php
@@ -21,7 +21,7 @@
            <span>{{ $event->start_at ? $event->start_at->format('Y-m-d (D)') : 'N/A' }}</span>
          </div>
          <div>
-           <i class="fas fa-dollar-sign"></i>
+           <i class="fas fa-credit-card"></i>
            <span> 一般: {{ $event->price }} NTD / 學生: {{ $event->price * (1 - $event->discount_rate) }} NTD </span>
          </div>
          <div style="padding-bottom: 8px;">
