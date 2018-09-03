e0d6dba1164b6313ad76a89301e4895014550240
Michael.K <michael34435@capslock.tw>
2018-09-03 13:12:05 +0800

    fix: IPNController

--- php/app/Http/Controllers/IPNController.php
+++ php/app/Http/Controllers/IPNController.php
@@ -17,26 +17,36 @@ class IPNController extends Controller
         $merchantId = env('PAYMENT_MERCHANT_ID');
         $ipnSecret  = env('PAYMENT_IPN_SECRET');
 
-        $ipnMode  = $request->input('ipn_mode');
-        $merchant = $request->input('merchant');
-        $hmac     = $request->header('hmac');
-
-        if ($ipnMode != 'hmac') {
-            throw new PaymentException(412, 'IPN Mode is not HMAC');
+        if (!isset($_POST['ipn_mode'], $_POST['merchant'], $_POST['status'], $_POST['status_text'])) {
+            throw new PaymentException(412, 'Insufficient POST data provided.');
         }
 
-        if (!$hmac) {
-            throw new PaymentException(412, 'No HMAC signature sent.');
+        if ($_POST['ipn_mode'] == 'httpauth') {
+            if ($_SERVER['PHP_AUTH_USER'] !== $merchantId) {
+                throw new PaymentException(412, 'Invalid merchant ID provided.');
+            }
+
+            if ($_SERVER['PHP_AUTH_PW'] !== $ipnSecret) {
+                throw new PaymentException(412, 'Invalid IPN secret provided.');
+            }
+        } elseif ($_POST['ipn_mode'] == 'hmac') {
+            $hmac = hash_hmac('sha512', file_get_contents('php://input'), $ipnSecret);
+
+            if ($hmac !== $_SERVER['HTTP_HMAC']) {
+                throw new PaymentException(412, 'Invalid HMAC provided.');
+            }
+
+            if ($_POST['merchant'] !== $merchantId) {
+                throw new PaymentException(412, 'Invalid merchant ID provided.');
+            }
+        } else {
+            throw new PaymentException(412, 'Invalid IPN mode provided.');
         }
 
-        if ($merchant != trim($merchantId)) {
-            throw new PaymentException(412, 'No or incorrect Merchant ID passed');
-        }
+        $orderStatus = $_POST['status'];
 
-        $hmacHash = hash_hmac('sha512', $request->getContent(), trim($ipnSecret));
-        if (!hash_equals($hmac, $hmacHash)) {
-            throw new PaymentException(412, 'HMAC signature does not match');
-        }
+        // If $order_status is >100 or is 2, then it is complete
+        $orderStatus = ($orderStatus >= 100 || $orderStatus == 2)
 
         //These would normally be loaded from your database, the most common way is to pass the Order ID through the 'custom' POST field.
         $currency = env('PAYMENT_CURRENCY', 'TWD');
@@ -63,7 +73,7 @@ class IPNController extends Controller
             throw new PaymentException(412, 'Amount is less than order total!');
         }
 
-        if ($status >= 100 || $status == 2) {
+        if ($orderStatus) {
             // Send email to indicate you've paid the ticket
             Mail::to($order->email)->queue(new OrderConfirmed($order));
 
